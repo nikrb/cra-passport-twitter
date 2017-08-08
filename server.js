61133ba45c08dd2dd10b7c59ef02c25db62ec234
nikrb <nik_rb@yahoo.com>
2017-08-08 20:51:14 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+/**
+ * FIXME: do we need to sanitize anything?
+ */
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const app = express();
+const bodyParser = require('body-parser');
+
+var url = 'mongodb://localhost:27017/recipes';
+MongoClient.connect(url, function(err, db) {
+  if( err){
+    throw new Error( "mongo connect failed");
+  } else {
+    console.log( "connected to db");
+  }
+
+  app.set('port', (process.env.PORT || 8080));
+  app.use('/', express.static(process.cwd() + '/public'));
+
+
+  app.get( '/', (req,res) => {
+    res.sendFile( process.cwd() + '/public/index.html');
+  });
+
+
+  app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+  });
+});
