9853a00c3ea84dab0603a724816513570e381205
nikrb <nik_rb@yahoo.com>
2017-08-10 11:53:29 +0100

    add server passport auth routes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require( 'passport');
 require( 'dotenv').config();
-require( './server/models').connect( process.env.dbUri);
+require( './models').connect( process.env.dbUri);
 const app = express();
 
 app.set('port', (process.env.port || 8080));
@@ -10,16 +10,16 @@ app.use('/', express.static(process.cwd() + '/public'));
 app.use( bodyParser.urlencoded( {extended: false}));
 app.use( passport.initialize());
 
-const localSignupStrategy = require('./server/passport/local-signup');
-const localLoginStrategy = require('./server/passport/local-login');
+const localSignupStrategy = require('./passport/local-signup');
+const localLoginStrategy = require('./passport/local-login');
 passport.use('local-signup', localSignupStrategy);
 passport.use('local-login', localLoginStrategy);
 
-const authCheckMiddleware = require('./server/middleware/auth-check');
+const authCheckMiddleware = require('./middleware/auth-check');
 app.use('/api', authCheckMiddleware);
 
-const authRoutes = require('./server/routes/auth');
-const apiRoutes = require('./server/routes/api');
+const authRoutes = require('./routes/auth');
+const apiRoutes = require('./routes/api');
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
