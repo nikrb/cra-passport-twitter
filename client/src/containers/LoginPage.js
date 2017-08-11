3dde4c637310cf4dd449df0479e3de88625d3f9c
nikrb <nik_rb@yahoo.com>
2017-08-11 23:39:18 +0100

    fix login page to redirect to referer

--- client/src/containers/LoginPage.js
+++ client/src/containers/LoginPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {Redirect} from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 import Actions from './Actions';
 import Auth from '../modules/Auth';
@@ -7,8 +7,13 @@ import Auth from '../modules/Auth';
 export default class LoginPage extends React.Component {
   state = {
     errors: {},
-    user: { email: "", password:""}
+    user: { email: "", password:""},
+    redirectToReferrer: false
   };
+  componentWillMount = () => {
+    console.log( "LoginPage props:", this.props);
+  };
+
   changeUser = ( event) => {
     // event target name not user name!
     const {name, value} = event.target;
@@ -23,14 +28,22 @@ export default class LoginPage extends React.Component {
     Actions.postLogin( {email, password})
     .then( (response) => {
       console.log( "login response:", response);
-      this.setState( { errors: {}});
       Auth.authenticateUser( response.token);
+      this.setState( { errors: {}, redirectToReferrer: true});
+      this.props.onLogin();
     })
     .catch( (err) => {
       console.error( "login failed:", err);
     });
   };
   render = () => {
+    const { from } = this.props.location.state || { from: { pathname: '/' } };
+    const { redirectToReferrer } = this.state;
+    if (redirectToReferrer) {
+      return (
+        <Redirect to={from}/>
+      );
+    }
     return (
       <LoginForm onSubmit={this.processForm} onChange={this.changeUser}
         user={this.state.user} errors={this.state.errors} />
