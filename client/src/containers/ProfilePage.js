ed2c76a110048267ee113fe94c017f48d67dc486
nikrb <nik_rb@yahoo.com>
2017-08-15 11:49:27 +0100

    check user name and email

--- client/src/containers/ProfilePage.js
+++ client/src/containers/ProfilePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Auth from '../modules/Auth';
 
 export default class ProfilePage extends React.Component {
   render = () => {
+    const name = Auth.getUsername();
+    const email = Auth.getEmail();
+    // temporary display page, just center this
+    const centre = { textAlign: "center"};
     return (
-      <h1>Profile</h1>
+      <div style={centre}>
+        <h1>Profile</h1>
+        <div>Name: {name}</div>
+        <div>Email: {email}</div>
+      </div>
     );
   };
 }
